f438a5d38335ef15f71ebb24fd21811f5db17ea1
nordluma <maxnordlund.95@hotmail.com>
2023-11-01 23:07:30 +0200

    initial commit

---
+++ test/password_gen_test.exs
@@ -0,0 +1,61 @@
+defmodule PasswordGenTest do
+  use ExUnit.Case
+  doctest PasswordGen
+
+  setup do
+    options = %{
+      "length" => "10",
+      "numbers" => "false",
+      "uppercase" => "false",
+      "symbols" => "false"
+    }
+
+    options_type = %{
+      lowercase: Enum.map(?a..?z, &<<&1>>),
+      numbers: Enum.map(0..9, &Integer.to_string(&1)),
+      uppercase: Enum.map(?A..?Z, &<<&1>>),
+      symbols: String.split("!#$%&()*+,-./:;<=>?@[]^_{}|~", "", trim: true)
+    }
+
+    {:ok, result} = PasswordGen.generate(options)
+
+    %{
+      options_type: options_type,
+      result: result
+    }
+  end
+
+  test "returns a string", %{result: result} do
+    assert is_bitstring(result)
+  end
+
+  test "returns error when no length is given" do
+    options = %{"invalid" => "false"}
+
+    assert {:error, _error} = PasswordGen.generate(options)
+  end
+
+  test "returns error when length is not an integer" do
+    options = %{"length" => "abc"}
+
+    assert {:error, _error} = PasswordGen.generate(options)
+  end
+
+  test "length of returned string is the same as in the passed option" do
+    length_option = %{"lenght" => "5"}
+    {:ok, result} = PasswordGen.generate(length_option)
+
+    assert 5 = String.length(result)
+  end
+
+  test "return a lowercase string with the length", %{options_type: options} do
+    length_option = %{"length" => "5"}
+    {:ok, result} = PasswordGen.generate(length_option)
+
+    assert String.contains?(result, options.lowercase)
+
+    refute String.contains?(result, options.numbers)
+    refute String.contains?(result, options.uppercase)
+    refute String.contains?(result, options.symbols)
+  end
+end
